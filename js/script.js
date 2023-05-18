398f2e79f8c895c07dec414c4cefaa36688f406d
Brady <waltebra2001@gmail.com>
2023-05-18 14:48:17 -0500

    slacking

--- js/script.js
+++ js/script.js
@@ -10,18 +10,8 @@ $(document).ready(function() {
         $(this).removeClass("accent");
     };
 
-
-    let quarterHover = function() {
-        $(this).addClass("hovered-quarter");
-    }
-
-    let quarterUnhover = function() {
-        $(this).removeClass("hovered-quarter");
-    };
-
     let addEventListeners = function() {
         $(".navbar-item").hover(navItemHover, navItemUnhover);
-        $(".quarter").hover(quarterHover, quarterUnhover);
     };
 
     addEventListeners();
